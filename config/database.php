aaaa84114fccff63724f1aaad1b874f5f1f0ccdc
olalekanarowoselu <olalekanarowoselu@yahoo.co.uk>
2019-10-29 18:25:21 +0100

    more database configuration for aws

--- config/database.php
+++ config/database.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Str;
 
-define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
-define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
-define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
-define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
+//define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
+//define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
+//define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
+//define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
 
 return [
 
